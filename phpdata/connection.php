2e950de2b9cf679073477cb644c5edf12e459709
Thomas Ken Ronaldi <thomken21@outlook.com>
2021-04-16 02:41:52 +0700

    created form with docker compose

---
+++ phpdata/connection.php
@@ -0,0 +1,8 @@
+<?php 
+    $host = "db";
+    $user ="root";
+    $password = "viper63";
+    $database ="distributed_system";
+
+    $connection = mysqli_connect($host, $user, $password, $database);
+?>
